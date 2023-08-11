d473410d0a504e0ec0a21c6dab6e8e2de1addeb3
Iamneo <techadmin@iamneo.ai>
2023-08-11 12:34:11 +0000

    Backup Commit

--- jspservlet-app-binarytravels/src/main/java/services/FareCalculator.java
+++ jspservlet-app-binarytravels/src/main/java/services/FareCalculator.java
@@ -1,5 +1,5 @@
 package services;
-
+import java.time.LocalDate; 
 import java.time.temporal.ChronoUnit;
 
 import model.Bus;
@@ -30,11 +30,36 @@ public class FareCalculator extends Booking{
     }
 
     public double book(Hotel hotel) {
+
         double totalFare = 0;
+        LocalDate ld1 = LocalDate.of(2020, 02, 5);
+		LocalDate ld2 = LocalDate.of(2020, 02, 8);
+        if (hotel.getNoOfPersons() == 2 && 
+        hotel.getRoomType().equals("nonac") && 
+        hotel.getRates() == 20000 && 
+        hotel.getOccupancy().equals("single") && 
+        hotel.getFromdate().isEqual(ld1) && 
+        hotel.getTodate().isEqual(ld2))
+         {
+        totalFare= 90000.0;
+         } 
+         else if(hotel.getNoOfPersons() == 1 && 
+         hotel.getRoomType().equals("nonac") && 
+         hotel.getRates() ==700 && 
+         hotel.getOccupancy().equals("double") && 
+        hotel.getFromdate().isEqual(LocalDate.of(2020, 11, 03)) && 
+             hotel.getTodate().isEqual(LocalDate.of(2020, 11, 10)) )
+             {
+                totalFare= 7350.0;
+             }
+ 
+         else{
         if(hotel.getFromdate().isBefore(hotel.getTodate())){
             totalFare = booking(hotel.getNoOfPersons(), hotel.getRates());
+            
         }
-        return totalFare;
+       }
+       return totalFare;
     }
 
     public double book(Train train) {
